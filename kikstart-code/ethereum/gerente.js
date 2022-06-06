f76daa38a1e785efb4791d291fb7073e69cfb153
jauberth <jauberth@uesc.br>
2022-06-06 09:59:52 -0300

    Update gerente.js

--- kikstart-code/ethereum/gerente.js
+++ kikstart-code/ethereum/gerente.js
@@ -3,7 +3,7 @@ import GerenteDeContratos from './build/GerenteDeContratos.json';
 
 const instancia = new web3.eth.Contract(
   JSON.parse(GerenteDeContratos.interface),
-  '0xc9910b1d2CEdC56faEa6316e555642d4A18f1037'
+  'endereço do contrato'
 );
 
 export default instancia;
