f676f88535d667b4120e0d1be68655dcba8db7c5
jauberth <jauberth@uesc.br>
2022-06-06 10:00:28 -0300

    Update web3.js

--- kikstart-code/ethereum/web3.js
+++ kikstart-code/ethereum/web3.js
@@ -6,7 +6,7 @@ if (typeof window !=='undefined' && typeof window.web3 !=='undefined') {
   web3 = new Web3(window.web3.currentProvider);
 } else {
   const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/d0acdacba51b456b88d07452e0ab2a89'
+    'https://rinkeby.infura.io/v3/endereço do infura'
   );
   web3 = new Web3(provider);
 }
